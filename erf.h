73f4da5f8573e79069c49e9fa7c412b5e1d87e94
AndrewCRMartin <andrew@bioinf.org.uk>
2015-07-18 15:15:53 +0100

    erf.h - Header file for numerics error function

---
+++ erf.h
@@ -0,0 +1,6 @@
+double erff(double x);
+double erff(double x);
+double gammp(double a, double x);
+void   gser(double *gamser, double a, double x, double *gln);
+double gammln(double xx);
+void   gcf(double *gammcf, double a, double x, double *gln);
